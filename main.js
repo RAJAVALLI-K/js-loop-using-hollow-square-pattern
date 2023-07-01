ae0d4394737b9180ae22d799ef8a956815031a15
RAJAVALLI-K <rajavalli1932bca@gmail.com>
2023-07-01 21:39:14 +0530

    add hollow square pattern

---
+++ main.js
@@ -0,0 +1,21 @@
+//1. Hollow square pattern
+
+// let num = 5;
+// value = '';
+// for (let i = 0; i < num; i++) {
+//   for (let j = 0; j < num; j++) {
+//     if (i == 0 || i == num - 1) {
+//       value += '*';
+//     }
+//     else {
+//       if (j == 0 || j == num - 1) {
+//         value += '*';
+//       }
+//       else {
+//         value += ' ';
+//       }
+//     }
+//   }
+//   value += '\n';
+// }
+// console.log(value);
